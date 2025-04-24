fdbb3fca930da6a4eeea253273ec60e85c087b40
yazan <yazanmohamed16@gmail.com>
2025-04-24 19:27:27 +0300

    I do update of department table

---
+++ migrations/20250423072148_update_departmentCollege_table.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.alterTable('departments',function(table){
+      table.string('college_name').notNullable();
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.alterTable('departments',function(table){
+      table.dropColumn('college_name');
+  })
+};
