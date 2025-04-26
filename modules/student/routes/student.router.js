573868e5d8ecfc5f2279a205b2f6b8df3f61c1a7
yazan <yazanmohamed16@gmail.com>
2025-04-26 10:57:55 +0300

    I do update of student router

--- modules/student/routes/student.router.js
+++ modules/student/routes/student.router.js
@@ -7,12 +7,12 @@ const authorizeRoles=require('../../main/middelware/authorizeRoles.middleware')
 
 
 router.post('/signup',createStudentValidation,studentCreateByPost);
-router.get('/admin/all-student',Authenticated,authorizeRoles('admin'),studentListByGet);
+router.get('/admin/all-student',Authenticated,authorizeRoles('superadmin'),studentListByGet);
 router.get('/profile',Authenticated,authorizeRoles('student'),getStudentProfileByGet);
-router.delete('/admin/delete-student/:id',Authenticated,authorizeRoles('admin'),deleteStudentValidation,deleteStudentByDelete);
+router.delete('/admin/delete-student/:id',Authenticated,authorizeRoles('superadmin'),deleteStudentValidation,deleteStudentByDelete);
 router.put('/student/update',Authenticated,authorizeRoles('student'),updateStudentValidation,updateStudentByPut);
 router.put('/student/change-password',Authenticated,authorizeRoles('student'),changePasswordValidation,updatePasswordByPut);
-router.get('/admin/student/:id',Authenticated,authorizeRoles('admin'),studentByIdValidation,getStudentByIdByGet);
+router.get('/admin/student/:id',Authenticated,authorizeRoles('superadmin'),studentByIdValidation,getStudentByIdByGet);
 
 
 
