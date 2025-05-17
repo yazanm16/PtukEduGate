25c5544663c426af20895263c3c88df1229e06d0
yazan <yazanmohamed16@gmail.com>
2025-05-17 12:24:49 +0300

    I add routes for book table create ,delete and list books with filters, and update .

--- modules/books/routes/book.router.js
+++ modules/books/routes/book.router.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const Authenticated=require('../../main/middelware/auth.middelware')
 const authorizeRoles=require('../../main/middelware/authorizeRoles.middleware')
 const validate = require("../../main/middelware/handleValidation");
-const {createBookByPost, getBookByGet, deleteBookByDelete} = require("../controller/book.controller");
-const {createBookValidation, getBooksValidation, deleteBookValidation} = require("../validation/book.validation");
+const {createBookByPost, getBookByGet, deleteBookByDelete, updateBookByPut} = require("../controller/book.controller");
+const {createBookValidation, getBooksValidation, deleteBookValidation, updateBookValidation} = require("../validation/book.validation");
 const uploadBook=require('../middelware/book.multer')
 
 router.post('/admin/book-create',Authenticated,authorizeRoles('superadmin'),uploadBook.single('file'),createBookValidation,validate,createBookByPost);
@@ -13,5 +13,5 @@ router.get('/book',getBooksValidation,validate,getBookByGet)
 
 router.delete('/admin/book-delete/:book_id',Authenticated,authorizeRoles('superadmin'),deleteBookValidation,validate,deleteBookByDelete)
 
-router.put('/admin/book-update/:book_id')
+router.put('/admin/book-update/:book_id',Authenticated,authorizeRoles('superadmin'),updateBookValidation,validate,updateBookByPut)
 module.exports = router;
