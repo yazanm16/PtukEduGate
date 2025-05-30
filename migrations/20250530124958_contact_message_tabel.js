4893d45d1dcdd15365f2126f2383053093ef00b1
yazan <yazanmohamed16@gmail.com>
2025-05-30 17:34:15 +0300

    Add migration for contact_messages table
    Introduce a new `contact_messages` table with fields for name, email, phone, message, and timestamps. This migration sets up the schema to store user inquiries or contact data.

---
+++ migrations/20250530124958_contact_message_tabel.js
@@ -0,0 +1,22 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.createTable('contact_messages', function(table) {
+        table.increments('id').primary();
+        table.string('name').notNullable();
+        table.string('email').notNullable();
+        table.string('phone').nullable();
+        table.text('message').notNullable();
+        table.timestamp('created_at').defaultTo(knex.fn.now());
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('contact_messages');
+};
