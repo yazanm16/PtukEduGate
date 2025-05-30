fb704cf9b7c9f1f285e531ae1368003adfc6d1e1
yazan <yazanmohamed16@gmail.com>
2025-05-30 17:37:47 +0300

    Update phone validation by removing strictMode option
    Removed the strictMode setting from the phone validation logic, simplifying the mobile phone validation for 'ar-PS' locale. This should allow for broader compatibility with valid phone number formats.

--- modules/contact_messages/validation/contact.validation.js
+++ modules/contact_messages/validation/contact.validation.js
@@ -6,7 +6,7 @@ const db = knex(knexConfig);
 const name=body('name').notEmpty().withMessage('The name is required');
 const email=body('email').isEmail().withMessage('The email is required');
 const message=body('message').notEmpty().withMessage('The message is required');
-const phone=body('phone').optional().isMobilePhone('ar-PS', {strictMode:true}).withMessage('Invalid phone number');
+const phone=body('phone').optional().isMobilePhone('ar-PS',).withMessage('Invalid phone number');
 
 const sendContactMessageValidation=[
     name,
